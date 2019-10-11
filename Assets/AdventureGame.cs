5e88f03fa69eb252ff36ac4a1ffc36b77d2eb7b3
JingleDjango95 <spencerweber019@gmail.com>
2019-10-11 17:54:10 -0600

    Made conditionals for button presses

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-    
+
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
@@ -20,6 +21,21 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            state = nextStates[0];
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            state = nextStates[1];
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            state = nextStates[2];
+
+        textComponent.text = state.GetStateStory();
     }
 }
