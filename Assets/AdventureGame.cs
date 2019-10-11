d60523c2bcdb7a016964ab4911d4f230cd9c8383
JingleDjango95 <spencerweber019@gmail.com>
2019-10-11 11:49:38 -0600

    Made a textComponent

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+    
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
@@ -10,7 +10,7 @@ public class AdventureGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = ("You awaken, blinded and cold, the smell of mildew ...");
     }
 
     // Update is called once per frame
